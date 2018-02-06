f885ebc9e788be9067998576d593c2a86d7d059d
mseatris <mseatris@172.16.83.148>
2018-02-05 17:54:18 -0800

    testing

---
+++ SampleEJB/ejbModule/com/seatris/ejb/Testejb.java
@@ -0,0 +1,20 @@
+package com.seatris.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Testejb
+ */
+@Stateless
+@LocalBean
+public class Testejb {
+
+    /**
+     * Default constructor. 
+     */
+    public Testejb() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
